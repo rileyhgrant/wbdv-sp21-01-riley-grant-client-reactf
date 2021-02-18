606cf28ccd2c6811492068a102a010b9a8da5317
Riley Grant <grantrileyh@gmail.com>
2021-02-18 10:37:06 -0500

    Added files for several components and services, no proper work done yet

---
+++ src/components/course-editor.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const CourseEditor = () =>
+    <h1>Course Editor Yah</h1>
+// comment so vs-code doesn't auto indent
+
+// this one is more wordy! you can the line 3 syntax if its a 1 line return statement
+// const CourseEditor = () => {
+//     return (
+//         <h1>Course Editor!</h1>
+//     );
+// }
+
+export default CourseEditor;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* React main render function */
 ReactDOM.render(
   <React.StrictMode>
     <App />

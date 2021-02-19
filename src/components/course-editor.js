d3c05ebcca71d2105fb7808ad2bbf564687f52cf
Riley Grant <grantrileyh@gmail.com>
2021-02-19 09:48:45 -0500

    Added placeholder editor page that gets history and has functional back button

--- src/components/course-editor.js
+++ src/components/course-editor.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
-const CourseEditor = () =>
-    <h1>Course Editor Yah</h1>
+const CourseEditor = ({history}) =>
+    <h1>
+        {/* <Link to="/manager/table">
+            <i className = "fas fa-arrow-left"></i>
+        </Link> */}
+        <span onClick={() => history.goBack()}>
+            <i className = "fas fa-arrow-left"></i>
+        </span>
+        __Course Editor Yah
+    </h1>
 // comment so vs-code doesn't auto indent
 
 // this one is more wordy! you can the line 3 syntax if its a 1 line return statement

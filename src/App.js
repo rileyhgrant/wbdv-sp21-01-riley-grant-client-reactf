8f013edf55cb4132e8157d53cbef69b603c2ec7c
Riley Grant <grantrileyh@gmail.com>
2021-02-22 21:16:03 -0500

    App.js functional and complete

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import CourseManager from './components/course-manager'
-import CourseEditor from './components/course-editor'
+import CourseEditor from './components/course-editor/course-editor'
+import Home from "./components/home"
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+    <div className="container-fluid">
+      <Route path="/" exact={true}><Home/></Route>
       <Route path="/manager" component={CourseManager}/>
-      {/* <Route path="/editor" component={CourseEditor}/> */}
       <Route path="/editor" render={ (props) => <CourseEditor{...props}/> } />
-
-
-      {/* <div className="container-fluid">
-        Hello World from App!!!
-        <CourseManager/>
-        <CourseEditor/>
-      </div> */}
+    </div>
+      
     </BrowserRouter>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
